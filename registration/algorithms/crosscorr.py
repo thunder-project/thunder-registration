1273242bf271f2301a21f15d60194c78fb3861e8
freeman <the.freeman.lab@gmail.com>
2016-04-08 15:56:31 -0400

    doctoring

--- registration/algorithms/crosscorr.py
+++ registration/algorithms/crosscorr.py
@@ -8,6 +8,12 @@ from ..transforms import Displacement, LocalDisplacement
 class CrossCorr(object):
     """
     Registration using cross correlation.
+
+    Attributes
+    ----------
+    axis : integer
+        An axis to restrict displacements to, e.g. use axis=2 to
+        restrict displacements to axes (0, 1)
     """
     def __init__(self, axis=None):
         self.axis = axis
@@ -20,11 +26,11 @@ class CrossCorr(object):
 
     def fit(self, images, reference=None):
         """
-        Perform registration by computing integer
-        displacements between an image or volume and reference
-        using cross correlation.
+        Estimate registration model using cross-correlation.
 
-        Displacements will be 2D for images and 3D for volumes.
+        Use cross correlation to compute displacements between 
+        images or volumes and reference. Displacements will be 
+        2D for images and 3D for volumes.
 
         Parameters
         ----------
@@ -39,7 +45,7 @@ class CrossCorr(object):
 
         def func(item):
             key, image = item
-            return asarray([key, self.get(image, reference)])
+            return asarray([key, self._get(image, reference)])
 
         transformations = images.map(func, with_keys=True).toarray()
         if images.shape[0] == 1: 
@@ -49,12 +55,27 @@ class CrossCorr(object):
         return RegistrationModel(dict(transformations), algorithm=algorithm)
 
     def fit_and_transform(self, images, reference=None):
+        """
+        Estimate and apply registration model using cross-correlation.
+
+        Use cross correlation to compute displacements between 
+        images or volumes and reference, and apply the
+        estimated model to the data. Displacements will be 
+        2D for images and 3D for volumes.
+
+        Parameters
+        ----------
+        images : array-like or thunder images
+            The sequence of images / volumes to register.
+
+        reference : array-like
+            A reference image to align to.
+        """
         images = check_images(images)
         check_reference(images, reference)
 
         def func(image):
-            t = self.get(image, reference)
+            t = self._get(image, reference)
             return t.apply(image)
 
         return images.map(func)
-
